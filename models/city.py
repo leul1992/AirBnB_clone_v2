a2ff455860090d8a9dd7aa7677f943b0f02c69c0
HaxorLeoul <workinehw94@gmail.com>
2022-09-14 10:35:08 -0700

    Commit

--- models/city.py
+++ models/city.py
@@ -1,23 +1,24 @@
-#!/usr/bin/python3
-'''
-    Define the class City.
-'''
-from models.base_model import BaseModel, Base
+#!/usr/bin/python
+""" holds class City"""
 import models
-from os import environ
-from sqlalchemy import Column, String, Integer, DateTime, ForeignKey
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class City(BaseModel, Base):
-    '''
-        Define the class City that inherits from BaseModel.
-    '''
-    __tablename__ = "cities"
-    if environ.get("HBNB_TYPE_STORAGE") == "db":
+    """Representation of city """
+    if models.storage_t == "db":
+        __tablename__ = 'cities'
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-        places = relationship("Place", backref="cities", cascade="delete")
+        places = relationship("Place", backref="cities")
     else:
-        name = ""
         state_id = ""
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes city"""
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-'''
-    Implementation of the State class
-'''
-
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from models.city import City
-from os import environ
+""" holds class State"""
 import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    '''
-        Implementation for the State.
-    '''
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-
-    if environ.get("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="delete")
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
         name = ""
 
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            city_dict = models.storage.all(models.classes["City"])
+            """getter for list of city instances related to the state"""
             city_list = []
-
-            for key, value in city_dict.items():
-                if value.state_id == self.id:
-                    city_list.append(value)
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
             return city_list
