360d1a919f8dfa85e5f77d1c961edfc6741dc4de
Leulseged <leulbekele191@gmail.com>
2022-09-29 21:41:53 +0300

    compress

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 def do_pack():
     """script generates .tgz archive form web_static content"""
     date = datetime.now().strftime('%Y%m%d%H%M%S')
-    file_path = f'versions/web_static_{date}'
+    file_path = 'versions/web_static_{}.tgz'.format(date)
     local('mkdir /versions/')
     gen = local(f'tar -cvzf {file_path} web_static')
     if gen.succeeded):
