477dc134b8b466eab0fdcad0b32ba0dd443e8a43
Leulseged <leulbekele191@gmail.com>
2022-09-29 22:33:00 +0300

    web diploy

--- fabfile.py
+++ fabfile.py
@@ -1,16 +1,3 @@
-#!/usr/bin/env python3
-"""fabric script"""
-from fabric.api import local
-from datetime import datetime
-
-
-def do_pack():
-    """script generates .tgz archive form web_static content"""
-    date = datetime.now().strftime('%Y%m%d%H%M%S')
-    file_name = 'versions/web_static_{}.tgz'.format(date)
-    local('mkdir -p versions/')
-    gen = local('tar -cvzf {} web_static'.format(file_name))
-    if (gen.succeeded):
-        return file_name
-    else:
-        return None
+#!/usr/bin/python3
+def hello(name="world"):
+    print("Hello %s!" % name)
