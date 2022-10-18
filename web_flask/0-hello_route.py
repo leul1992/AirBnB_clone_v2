1ee1bf1996c28379bff67ac6b620e498f06b030b
Leulseged <leulbekele191@gmail.com>
2022-10-18 23:41:15 +0300

    hello flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
+"""module starts flask web application"""
 from flask import Flask
 app = Flask(__name__)
-"""module starts flask web application"""
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ hello_hbnb method """
     return ('Hello HBNB!')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
