ce4faa366fcd8bc63be465f1d6ed16e7be88037f
Leulseged <leulbekele191@gmail.com>
2022-10-19 16:56:54 +0300

    c is fun

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""module starts flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def pri():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def pri2():
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def pri3(text):
+    return "C %s" % text
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
