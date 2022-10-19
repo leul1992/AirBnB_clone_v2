63a6df5ac73d3e697a59237c2f5d99f989c3e87a
Leulseged <leulbekele191@gmail.com>
2022-10-19 17:18:24 +0300

    number only

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""module starts flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def pri():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def pri2():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def pri3(text):
+    text = text.replace('_', ' ')
+    return "C %s" % text
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pri4(text=None):
+    if text is None:
+        text = "is cool"
+    else:
+        text = text.replace('_', ' ')
+
+    return "Python %s" % text
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def pri_num(n):
+    return '%d is a number' % n
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
